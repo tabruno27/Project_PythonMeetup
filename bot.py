cd4a53ad61e7c79c6a704f4bd80e22588d0bc884
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-05-20 13:31:52 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+
+# Команда /start
+def start():
+
+  
+# Команда /ask_question
+def ask_question():
+  # Реализация функции "пользователь задал вопрос" 
+
+  
+# Команда /get_program
+def get_program():
+    # Здесь нужно получить программу мероприятия из базы данных
+
+
+# Команда /notify_users
+def notify_users(self):
+    # Оповещение о смене программы
+
+  
+# Основная функция
+def main():
+
+
+if __name__ == "__main__":
+    main()

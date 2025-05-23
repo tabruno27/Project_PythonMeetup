b13a95c03015963fa5d412beeab0e9855969334b
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-05-23 18:24:23 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,9 +3,15 @@ from .scheduler import register_scheduler_handlers
 from .questions import register_question_handlers
 from .start import register_start_handlers
 from .active import register_active_handlers
+from .create_speaker_ad import register_create_speaker_ad_handlers
+from .delete_speaker_ad import register_delete_speaker_ad_handlers
+from .updat_scheduler_ad import register_up_sheduler_handlers
 
 def register_all_handlers(dp: Dispatcher):
     register_start_handlers(dp)
     register_scheduler_handlers(dp)
     register_question_handlers(dp)
     register_active_handlers(dp)
+    register_create_speaker_ad_handlers(dp)
+    register_delete_speaker_ad_handlers(dp)
+    register_up_sheduler_handlers(dp)

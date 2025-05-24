b169717ad08425a469538bd7a7a51253fc165df9
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-05-24 14:12:02 +0500

    clear code

--- handlers/my_questions.py
+++ handlers/my_questions.py
@@ -2,9 +2,7 @@ import os
 from aiogram import types, Dispatcher
 from aiogram.filters import Command
 from dotenv import load_dotenv
-from datacenter.db_manager import get_speaker_by_telegram_id, get_talks_by_speaker, get_questions_for_talk, \
-    mark_question_as_answered
-import logging
+from datacenter.db_manager import get_speaker_by_telegram_id, get_talks_by_speaker, get_questions_for_talk
 
 load_dotenv()
 ORGANIZER_TELEGRAM_ID = int(os.getenv('ORGANIZER_TELEGRAM_ID'))
@@ -50,38 +48,6 @@ async def handle_my_questions(message: types.Message):
 
     await message.answer("\n".join(result_message))
 
-    # await message.answer(
-    #     "Чтобы отметить вопрос как отвеченный, используйте команду:\n"
-    #     "/answered <ID вопроса>\n\n"
-    #     "Пример: /answered 5"
-    # )
-
-
-# async def handle_answered(message: types.Message):
-#     user_id = message.from_user.id
-#
-#     speaker = get_speaker_by_telegram_id(user_id)
-#
-#     if not speaker:
-#         await message.answer("❌ Только зарегистрированные спикеры могут отмечать вопросы как отвеченные.")
-#         return
-#
-#     command_parts = message.text.split()
-#     if len(command_parts) != 2 or not command_parts[1].isdigit():
-#         await message.answer("❌ Неверный формат команды. Используйте: /answered <ID вопроса>")
-#         return
-#
-#     question_id = int(command_parts[1])
-#
-#     updated_question = mark_question_as_answered(question_id)
-#
-#     if updated_question:
-#         await message.answer(f"✅ Вопрос #{question_id} отмечен как отвеченный!")
-#     else:
-#         await message.answer(f"❌ Вопрос с ID {question_id} не найден или уже отмечен как отвеченный.")
-
 
 def register_my_questions_handlers(dp: Dispatcher):
-    """Регистрация обработчиков просмотра вопросов"""
     dp.message.register(handle_my_questions, Command("my_questions"))
-    # dp.message.register(handle_answered, Command("answered"))

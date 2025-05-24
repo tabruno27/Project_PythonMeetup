79ba51cd77a2ec125df906e3a32df4767230259a
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-05-24 10:12:45 +0500

    add create_table() add try-except-finally to start_polling

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from handlers import register_all_handlers
+from datacenter.db_manager import create_tables
 
 
 async def set_bot_commands(bot: Bot):
@@ -22,18 +23,32 @@ async def set_bot_commands(bot: Bot):
 
 async def main():
     load_dotenv()
-    TG_TOKEN = os.getenv('TG_TOKEN')
+    tg_token = os.getenv('TG_TOKEN')
 
-    bot = Bot(token=TG_TOKEN)
+    if not tg_token:
+        logging.error("Ошибка: Токен бота не найден в переменных окружения")
+        return
+
+    create_tables()
+
+    bot = Bot(token=tg_token)
     dp = Dispatcher()
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    logging.info("Запуск бота...")
 
     await set_bot_commands(bot)
-
     register_all_handlers(dp)
 
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        logging.error(f"Ошибка при запуске бота: {e}")
+    finally:
+        await bot.session.close()
 
 
 if __name__ == '__main__':
